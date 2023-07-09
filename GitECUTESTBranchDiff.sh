c51cb5905b60cf9d1b4eee6a8bc237c855579103
jonathanAnguise <jonathan.anguise@protonmail.com>
2023-07-09 19:38:20 +0200

    Create tmp repo and get  list of changed file.
    * Create a tmp file with the date option where
    file will be copied in order to be a diff.
    * Save changed files in a variable in order to
    continue the analysis.

--- GitECUTESTBranchDiff.sh
+++ GitECUTESTBranchDiff.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#!/bin/bash -xv
+
+# Save current path
+CURRENT_PATH=$(pwd)
+echo $CURRENT_PATH
+
+# Create tmp file
+#mkdir $(date "+tmp_%N")
+
+# Go to the path
+cd $1
 
 # Function to display help
 git_ecu_test_branch_diff_help() {
@@ -6,7 +16,7 @@ git_ecu_test_branch_diff_help() {
     echo "Description: This function accepts four arguments and performs a specific task."
     echo ""
     echo "Arguments:"
-    echo "  arg1: Description of argument 1"
+    echo "  arg1: Path of the repo"
     echo "  arg2: Description of argument 2"
     echo "  arg3: Description of argument 3"
     echo ""
@@ -27,9 +37,11 @@ git_ecu_test_branch_diff() {
     echo "Argument 2: $2"
     echo "Argument 3: $3"
 
-    # Perform the desired task using the arguments
+    # get the list of the file that changed
+    file_changed_list=$(git --no-pager  diff --name-only master experimental_branch)
     
     echo $(git log)
 }
 
 git_ecu_test_branch_diff
+cd -
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # GitECUTESTBranchDiff
 
+remember you have created this git repo for tests ../test_to_be_deleted_with_2_git_repo/repo_experimental/
+It contains 2 branch. 
+   1. master
+   2. experimental_branch
+
+KAYZEN
+1.[x] create a tmp directory unic
+2.[x] give it the path and go on the path
+2.[ ] Get branches to review and branches source
+3.[ ] create a list of files modified between two branches
+4.[ ] copy this file in the tmp directory
+5.[ ] Checkout this file
+6.[ ] Do the command with theses two files
